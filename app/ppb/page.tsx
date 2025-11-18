2619a123749e5664a92bc938e3626e2069e9fb32
Stephanie Rose <stephanierose@Stephanies-MacBook-Pro.local>
2025-11-18 08:21:38 -0700

    Add Clarity tracking script directly to PPB page

--- app/ppb/page.tsx
+++ app/ppb/page.tsx
@@ -1106,6 +1106,17 @@ export default function PPBPage() {
           </div>
         </section>
       </main>
+      
+      {/* Microsoft Clarity Tracking */}
+      <Script id="clarity-ppb" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "u7rqk5v9bp");
+        `}
+      </Script>
     </div>
   );
 }
